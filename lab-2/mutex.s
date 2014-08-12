e11147f62b87e594c319fc03f0cc78610fb1005d
skylinen4s <skyline_n4s@hotmail.com>
2014-08-12 19:33:05 +0800

    implement mutex using assembly

--- lab-2/mutex.s
+++ lab-2/mutex.s
@@ -8,9 +8,13 @@
 	.global lock_mutex
 	.type lock_mutex, function
 lock_mutex:
-        @ INSERT CODE BELOW
-
-        @ END CODE INSERT
+	mov r1, locked		@ load locked value
+.L1:
+	ldrex r2, [r0]
+	cmp r2, unlocked 	@ compare to unlocked value, 
+	strexeq r2, r1, [r0]	@ if equal(r2 unlocked), store r1 into [r0]
+	cmpeq r2, #0		@ if success stored, return 0 to r2. Here to check it
+	bne .L1
 	bx lr
 
 	.size lock_mutex, .-lock_mutex
@@ -18,9 +22,10 @@ lock_mutex:
 	.global unlock_mutex
 	.type unlock_mutex, function
 unlock_mutex:
-	@ INSERT CODE BELOW
-        
-        @ END CODE INSERT
+	@ store unlocked value into r1 from [r0] to unlock
+        ldr r1, =unlocked
+	dmb			@ memory barrier
+	str r1, [r0]
 	bx lr
 	.size unlock_mutex, .-unlock_mutex
 
